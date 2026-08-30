80d8ab71af0089eb0329969df08b3d32e7a4025b
Delmy Bajxac <dgbajxac@correo.url.edu.gt>
2026-08-29 22:24:23 -0600

    proyecto semana 07-spring boot

---
+++ tablets-access-control/src/test/java/gt/edu/inebtls/tablets_access_control/TabletsAccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.edu.inebtls.tablets_access_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabletsAccessControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
